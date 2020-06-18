27e77fc01598f8c8cc6bfb8b8d1d86b6b20a4ec4
EC2 Default User <ec2-user@ip-172-31-34-105.ec2.internal>
2020-06-18 09:14:03 +0000

    test3

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,7 @@ export default {
     ** See https://axios.nuxtjs.org/options
     */
     axios: {
+        baseURL: 'https://appo.microcms.io/'
     },
     /*
     ** vuetify module configuration
@@ -135,10 +136,78 @@ export default {
             const data = await axios.get('https://appo.microcms.io/api/v1/content', {
                 headers: { 'X-API-KEY': '773389cb-ee15-43bb-ac24-0b97255ed891' }
             })
+            var contents = []
+            var menu = []
+            var posts = []
+            var post = {}
+            var items = []
+            var content = ''
+            var category = ''
+            var color = ''
+            var filterdPosts = []
+            
+            posts = data.contents.map((y, i) => {
+                post = {}
+                items = y.content.map((z, j) => {
+                    content = z.content
+                    if (z.item === 'i') {
+                        content = content.slice(13, content.length - 10)
+                    }
+                    return {
+                        id: j,
+                        type: z.item,
+                        xs: (z.xs)? z.xs : 12,
+                        sm: (z.sm)? z.sm : 12,
+                        md: (z.md)? z.md : 12,
+                        content: content
+                    }
+                })
+                if(y.category) {
+                    category = y.category.name
+                    color = y.category.color
+                }
+                else {
+                    category = ''
+                    color = ''
+                }
+                post = {
+                    id: y.id,
+                    params: y.menu.params,
+                    date: (y.date)? y.date : y.createdAt,
+                    title: y.title,
+                    overview: y.overview,
+                    category: category,
+                    color: category,
+                    content: items
+                }
+                menu = [...menu, y.menu]
+                return post
+            })
+            
+            menu = [...new Set(menu)]
+            
+            contents = menu.map(p => {
+                filterdPosts = posts.filter(q => q.params == p.params)
+                return {
+                    title: p.title,
+                    params: p.params,
+                    layout: p.layout,
+                    headerNav: p.headerNav,
+                    home: p.home,
+                    footerNav: p.footerNav,
+                    listItems: (p.listItems)? p.listItems : 0,
+                    number: (p.number)? p.number : 3, 
+                    xs: (p.xs)? p.xs : 12,
+                    sm: (p.sm)? p.sm : 12,
+                    md: (p.md)? p.md : 12,
+                    tab: p.tab,
+                    content: filterdPosts
+                }
+            })
+            
             var route = []
             var routing = []
-            
-            route = data.contents.map(x => {
+            route = this.contents.map(x => {
                 routing = x.content.map(y=>{
                     return `/${y.params}/${y.id}`
                 })
@@ -148,7 +217,11 @@ export default {
             .flat()
             
             return {
-                route: route
+                route: route,
+                payload: {
+                    menu: menu,
+                    contents: contents
+                }
             }
     
         }
