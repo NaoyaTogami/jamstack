56ba3dae4ab43682be86223495278ad9e0aafba4
EC2 Default User <ec2-user@ip-172-31-34-105.ec2.internal>
2020-06-18 13:13:26 +0000

    id page

--- pages/_category/_id.vue
+++ pages/_category/_id.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container :style="style">
-        {{ payload }}
+        {{ post }}
     </v-container>
 </template>
 
@@ -11,11 +11,10 @@ export default {
     components: {
         Post
     },
-    async asyncData ({ payload }) {
-        if (payload) {
-            return {
-                payload: payload
-            }
+    async asyncData ({ payload, store }) {
+        return {
+            menu: payload.menu,
+            contents: payload.contents
         }
     },
     data() {
@@ -31,6 +30,32 @@ export default {
         }
     },
     computed: {
+        post () {
+            var routing =  this.contents.map(x => {
+                return x.content.map(y=>{
+                    if (y.params == this.$route.params.category && y.id == this.$route.params.id) {
+                        return y
+                    }
+                })
+                .flat()
+            })
+            .flat()
+            routing =  routing.filter(z => z).flat()
+            return routing[0]
+        },
+        post () {
+            var routing =  this.contents.map(x => {
+                return x.content.map(y=>{
+                    if (y.params == this.$route.params.category && y.id == this.$route.params.id) {
+                        return y
+                    }
+                })
+                .flat()
+            })
+            .flat()
+            routing =  routing.filter(x => x).flat()
+            return routing[0]
+        },
         style () {
             if (this.$vuetify.breakpoint.mdAndUp) {
                 return 'margin-top:64px;'
