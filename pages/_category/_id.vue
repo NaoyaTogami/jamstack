f5e364151b15d4a5314338cb5efeb2c46d19f5ce
EC2 Default User <ec2-user@ip-172-31-34-105.ec2.internal>
2020-06-18 12:55:56 +0000

    test1

--- pages/_category/_id.vue
+++ pages/_category/_id.vue
@@ -1,19 +1,6 @@
 <template>
     <v-container :style="style">
         {{ payload }}
-        <v-sheet tile :color="color.bg">
-            <v-row no-gutters>
-                <v-col cols="12">
-                    <v-card flat tile :color="color.title.bg">
-                        <v-card-title :class="`title px-2 ${color.title.txt}`">
-                            {{ post.title }}
-                        </v-card-title>
-                    </v-card>
-                </v-col>
-            </v-row>
-            
-            <Post :post="post.content" :color="color" />
-        </v-sheet>
     </v-container>
 </template>
 
@@ -44,19 +31,6 @@ export default {
         }
     },
     computed: {
-        post () {
-            var routing =  this.contents.map(x => {
-                return x.content.map(y=>{
-                    if (y.params == this.$route.params.category && y.id == this.$route.params.id) {
-                        return y
-                    }
-                })
-                .flat()
-            })
-            .flat()
-            routing =  routing.filter(x => x).flat()
-            return routing[0]
-        },
         style () {
             if (this.$vuetify.breakpoint.mdAndUp) {
                 return 'margin-top:64px;'
